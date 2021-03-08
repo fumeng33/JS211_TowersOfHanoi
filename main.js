861dd2c0405f1f5c0533ebe884ff3712c9a3b96d
Meng Fu <Ellie@Ellies-Air.lan1>
2021-03-07 19:58:27 -0600

    push TOH

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ let stacks = {
 };
 
 // Start here. What is this function doing?
+// this function is will console log the stacks object array 
 const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
@@ -31,15 +32,12 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
-// This function passess in SS and ES to play the game by moving each pieces 
+// This function passess in startStack and endStack to check if the game is legal. we play the game by moving from start stack to end stack 
 const movePiece = (startStack, endStack) => {
   // Your code here
-
-
 console.log("START*", stacks[startStack])
 console.log("END*", stacks[endStack])
-
-// set avarible to move the end pieces .pop() method 
+// poping the smallest end array to the startStack off 
 const piece = stacks[startStack].pop()
 // popped the end pieces off and  pushed to the endstack 
 stacks[endStack].push(piece) 
@@ -47,6 +45,20 @@ console.log("piece", piece)
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
+//The first thing to check is if the startStack(the stack where the piece is moving from) has a piece to move. 
+
+// Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
+/**
+ * This function returns true if it is a legal move, and false if not legal.
+ * The first thing to check is if the startStack(the stack where the piece is moving from) has a piece to move. 
+ * Because you cannot move a piece that doesn't exist.
+ * Then we can test if the place we are moving to has no pieces. We do these two tests first because slice will be undefined if the arrays have no elements.
+ * IF(if statment) that is true, move on to next test. IF not true, return true.
+ * The next test is to check IF the peice being moved is smaller than the piece it is being stacked upon.
+ * Originally I tried to find a solution with .length - 1 but found it was easier to use slice(-1) to pull the end value of the array and compare them.
+ * If true return TRUE. Else return false.
+ */
+
 const isLegal = (startStack, endStack) => {
   // Your code here
 // if end stack is empty than can move or end stack is grater than start stack 
@@ -58,10 +70,12 @@ if ((!stacks[endStack].length) || stacks[startStack][stacks[startStack].length -
   return false;
 }
 }
-
 // What is a win in Towers of Hanoi? When should this function run?
 // to win the game you move stack  a to b or c with the index of [4,3,2,1]
+// we need to check if stack B or C is full of pieces and if they are true and full the moves will pass
+// the isLegal function will check the moves are legal 
 // pass in the function to tell the end users it's a win if the index of tower b and c is [4,3,2,1] YOU WON! 
+
 const checkForWin = () => {
   // Your code here
   
@@ -81,6 +95,13 @@ if (stacks['b'].length == 4 || stacks['c'].length == 4) {
 
 }
 // When is this function called? What should it do with its argument?
+// This function will make sure all the pieces are working in order to execute the game
+// at fist we check with a IF statement and check is the move legal 
+// ifLegal it is false console log try again 
+// ifLegal is true, move the pieces 
+// we will check for win if true and console log "YOU WON!"
+
+
 const towersOfHanoi = (startStack, endStack) => {
   // Your code here
   // if the condition isLegal function and movePiece are met return true or if not return 'try again'
